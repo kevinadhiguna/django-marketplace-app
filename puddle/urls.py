725b9eb6e39370b88957d82fbd780aae2431956e
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-25 11:34:17 +0700

    refactor(image): register static url

--- puddle/urls.py
+++ puddle/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 from core.views import index, contact # Import functions from views.py in the 'core' app
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # <- Just to show images in local (DO NOT do this in production!)
