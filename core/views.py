011834d31376eb1b099201e83e6499fb6857efe0
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-25 15:16:27 +0700

    refactor: define signup method

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from item.models import Category, Item
 
+from .forms import SignUpForm
+
 # Create your views here.
 def index(request):
   items = Item.objects.filter(is_sold=False)[0:6] # <- Display first six unsold items 
@@ -14,3 +16,10 @@ def index(request):
 
 def contact(request):
   return render(request, "core/contact.html")
+
+def signup(request):
+  form = SignUpForm()
+  
+  return render(request, 'core/signup.html', {
+    'form': form,
+  })
