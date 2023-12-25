d0a3b0c5145aed6473ea98b218f116ccc648b7b1
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-25 10:22:01 +0700

    feat(admin): add category model

--- item/admin.py
+++ item/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Category
+
 # Register your models here.
+admin.site.register(Category) # In order to show 'Category' model in the admin panel
