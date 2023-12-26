873c49719353be1366405270aacbf62af48a8aa5
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-26 11:12:20 +0700

    chore: register dashboard url

--- puddle/urls.py
+++ puddle/urls.py
@@ -22,5 +22,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # <- Just to show images in local (DO NOT do this in production!)
