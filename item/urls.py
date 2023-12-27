fe7caaedf12e74a4d1de9f9c3630e972f6f1435a
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-27 16:57:56 +0700

    fix: fix delete and edit endpoints

--- item/urls.py
+++ item/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
   path('', views.items, name='items'),
   path('new/', views.new, name='new'),
   path('<int:pk>/', views.detail, name='detail'),
-  path('<int:pk>/', views.delete, name='delete'),
-  path('<int:pk>/', views.edit, name='edit'),
+  path('<int:pk>/delete/', views.delete, name='delete'),
+  path('<int:pk>/edit/', views.edit, name='edit'),
 ]
