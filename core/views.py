080b3251e1d5bcaf8d3abbe4c0f7d78125ee57b2
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2023-12-27 16:57:10 +0700

    chore: change double quotes to single quotes for consistency

--- core/views.py
+++ core/views.py
@@ -9,13 +9,13 @@ def index(request):
   items = Item.objects.filter(is_sold=False)[0:6] # <- Display first six unsold items 
   categories = Category.objects.all()
 
-  return render(request, "core/index.html", {
+  return render(request, 'core/index.html', {
     'categories': categories,
     'items': items,
   })
 
 def contact(request):
-  return render(request, "core/contact.html")
+  return render(request, 'core/contact.html')
 
 def signup(request):
   if request.method == 'POST':
