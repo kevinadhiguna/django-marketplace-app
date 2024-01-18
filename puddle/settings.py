214fd031040fda1c83465ab08c4de6151f1be378
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2024-01-19 02:59:33 +0700

    chore: migrate to mysql and load environment variables

--- puddle/settings.py
+++ puddle/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv, find_dotenv
+
+# (1)
+env_filename = '.env'
+dotenv_path = join(dirname(dirname(__file__)), env_filename)
+load_dotenv(dotenv_path)
+
+# (2)
+# load_dotenv(find_dotenv())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,8 +94,12 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
